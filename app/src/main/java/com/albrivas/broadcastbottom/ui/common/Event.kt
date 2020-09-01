b39249b824635966edd56eda2ddd2193adea4d02
Alberto Rivas <alberto.rivas@avantgardeit.es>
2020-09-01 20:42:58 +0200

    Added class event to manage navigation correctly

---
+++ app/src/main/java/com/albrivas/broadcastbottom/ui/common/Event.kt
@@ -0,0 +1,18 @@
+package com.albrivas.broadcastbottom.ui.common
+
+/**
+ * Used as a wrapper for data that is exposed via a LiveData that represents an event.
+ */
+open class Event<out T>(private val content: T) {
+
+    private var hasBeenHandled = false
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+}
