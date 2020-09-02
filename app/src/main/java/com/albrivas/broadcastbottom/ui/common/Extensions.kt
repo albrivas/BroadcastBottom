97c966629b0c37627fce70cf8a2ea07f2ea3c4ea
Alberto Rivas <alberto.rivas@avantgardeit.es>
2020-09-02 12:30:40 +0200

    Create class extensions functions

---
+++ app/src/main/java/com/albrivas/broadcastbottom/ui/common/Extensions.kt
@@ -0,0 +1,12 @@
+package com.albrivas.broadcastbottom.ui.common
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: String) {
+    Toast.makeText(
+        this,
+        message,
+        Toast.LENGTH_SHORT
+    ).show()
+}
