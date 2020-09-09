67f896d0f0b670eac86fa8d5bfcf8407d5231757
Alberto Rivas <alberto@albrivas.com>
2020-09-09 14:09:34 +0200

    Added design and logic to view ResetPassword

---
+++ app/src/main/java/com/albrivas/broadcastbottom/ui/login/ResetPasswordFragment.kt
@@ -0,0 +1,65 @@
+package com.albrivas.broadcastbottom.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.albrivas.broadcastbottom.R
+import com.albrivas.broadcastbottom.databinding.FragmentResetPasswordBinding
+import com.albrivas.broadcastbottom.ui.common.Event
+import com.albrivas.broadcastbottom.ui.common.toast
+import org.koin.androidx.scope.lifecycleScope
+import org.koin.androidx.viewmodel.scope.viewModel
+
+class ResetPasswordFragment : Fragment() {
+
+    private lateinit var binding: FragmentResetPasswordBinding
+    private val viewModel: LoginViewModel by lifecycleScope.viewModel(this)
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentResetPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        instances()
+        observers()
+    }
+
+    private fun instances() {
+        binding.apply {
+            viewmodel = viewModel
+            lifecycleOwner = this@ResetPasswordFragment
+        }
+        navController = findNavController()
+    }
+
+    private fun observers() {
+        viewModel.model.observe(viewLifecycleOwner, Observer(::updateUi))
+    }
+
+    private fun updateUi(model: LoginViewModel.UiModel) {
+        when (model) {
+            is LoginViewModel.UiModel.NavigateSignIn -> navigateToSignIn(model.event)
+            is LoginViewModel.UiModel.ErrorLogin -> context?.toast(model.exception.message!!)
+        }
+    }
+
+    private fun navigateToSignIn(event: Event<String>) {
+        event.getContentIfNotHandled()?.let {
+            val action =
+                ResetPasswordFragmentDirections.actionResetPasswordFragmentToLoginFragment()
+            navController.navigate(action)
+        }
+    }
+
+}
