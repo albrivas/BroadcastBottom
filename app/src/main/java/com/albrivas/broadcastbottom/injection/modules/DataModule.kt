5343c5e8d79c1cfb9d2535d1a58c7e055e27a5e6
Alberto Rivas <alberto@albrivas.com>
2020-11-14 23:19:35 +0100

    chore: Added new module injection

--- app/src/main/java/com/albrivas/broadcastbottom/injection/modules/DataModule.kt
+++ app/src/main/java/com/albrivas/broadcastbottom/injection/modules/DataModule.kt
@@ -4,5 +4,5 @@ import com.albrivas.broadcastbottom.data.repository.UploadImageRepository
 import org.koin.dsl.module
 
 val dataModule = module {
-    factory { UploadImageRepository() }
+    factory { UploadImageRepository(get()) }
 }
