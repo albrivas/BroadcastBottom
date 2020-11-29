0fbab0849e66306ffef4e0c07aea238e5953339b
Alberto Rivas <alberto@albrivas.com>
2020-11-29 23:05:13 +0100

    chore: Show icon "delete" in notification screen

--- app/src/main/java/com/albrivas/broadcastbottom/ui/base/MainActivity.kt
+++ app/src/main/java/com/albrivas/broadcastbottom/ui/base/MainActivity.kt
@@ -6,9 +6,17 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.albrivas.broadcastbottom.R
 import com.albrivas.broadcastbottom.databinding.ActivityMainBinding
@@ -16,20 +24,31 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.scope.lifecycleScope
 import org.koin.androidx.viewmodel.scope.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     private val viewModel: MainViewModel by lifecycleScope.viewModel(this)
     private var countNotification = 0
+    private lateinit var menuProfile: Menu
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater, container, false)
         setContentView(binding.root)
+        configureToolbar()
         instances()
-        instancesNavigation()
+        configureNavigation()
         observers()
+    }
+
+    private fun configureToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(false)
+            setDisplayShowHomeEnabled(false)
+            setDisplayHomeAsUpEnabled(false)
+        }
     }
 
     private fun instances() {
@@ -39,12 +58,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun instancesNavigation() {
+    private fun configureNavigation() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_alerts,
+                R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNavigation.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener(this)
     }
 
     private fun observers() {
@@ -79,5 +107,42 @@ class MainActivity : AppCompatActivity() {
     private fun removeBadge() {
         countNotification = 0
         binding.bottomNavigation.removeBadge(R.id.navigation_alerts)
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        when (destination.id) {
+            R.id.navigation_alerts ->
+                binding.iconDelete.visibility =
+                    View.VISIBLE
+
+            R.id.navigation_profile -> {
+            }
+            else ->
+                binding.iconDelete.visibility =
+                    View.GONE
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuProfile = menu!!
+        menuInflater.inflate(R.menu.profile_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.close_session -> {
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        val a = menu?.hasVisibleItems()
+        return false
     }
 }
