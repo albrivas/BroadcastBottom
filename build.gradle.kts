76cfa0063cf42f5bb91db6fa7c9454222bea606c
Alberto Rivas <alberto@albrivas.com>
2022-11-13 17:40:35 +0100

    Add sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.sonarqube.gradle.SonarQubeExtension
+import java.util.Properties
+
 apply(plugin = "org.sonarqube")
 apply(plugin = "jacoco")
 apply(plugin = "com.github.ben-manes.versions")
@@ -45,3 +49,44 @@ tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+tasks.register("jacocoTestReport", JacocoReport::class) {
+    reports {
+        xml.required.set(true)
+        html.required.set(false)
+    }
+}
+
+fun gradleFileProperties(propertiesFileName: String) : Properties {
+    val properties = Properties()
+    val localProperties = File(rootDir, propertiesFileName)
+
+    if (localProperties.isFile) {
+        java.io.InputStreamReader(
+                java.io.FileInputStream(localProperties), com.google.common.base.Charsets.UTF_8
+        ).use { reader ->
+            properties.load(reader)
+        }
+    }
+    return properties
+}
+
+// region Sonarqube Configuration
+
+val sonarQubeExtension = project.extensions.getByName("sonarqube") as SonarQubeExtension
+val sonarqubeFile = "sonar.properties"
+val sonarProperties = gradleFileProperties(sonarqubeFile)
+
+sonarQubeExtension.properties {
+    property("sonar.projectName", sonarProperties.getProperty("projectName"))
+    property("sonar.projectKey", sonarProperties.getProperty("projectKey"))
+    property("sonar.host.url", sonarProperties.getProperty("hostUrl"))
+    property("sonar.language", sonarProperties.getProperty("language"))
+    property("sonar.sources", sonarProperties.getProperty("sources"))
+    property("sonar.login", sonarProperties.getProperty("loginToken"))
+    property("sonar.test", sonarProperties.getProperty("testSource"))
+    property("sonar.java.coveragePlugin", sonarProperties.getProperty("coveragePlugin"))
+    property("sonar.android.lint.report", sonarProperties.getProperty("lintReport"))
+}
+
+//endregion
